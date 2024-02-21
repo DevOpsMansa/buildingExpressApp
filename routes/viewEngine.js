ae76c830228da2be503819e20dc8adcc821931d8
Eric (Mansa) Marah <devopsmansa@gmail.com>
2024-02-21 16:45:07 -0600

    express pug

--- routes/viewEngine.js
+++ routes/viewEngine.js
@@ -33,3 +33,29 @@ const renderedHtml = renderView(data);
 
 // Output the rendered HTML
 console.log(renderedHtml);
+
+
+//use the templates with the Mustache.js view engine by loading the respective template file and rendering it with dynamic data
+
+const fs = require('fs');
+const mustache = require('mustache');
+
+// Load template files
+const template = fs.readFileSync('template.html', 'utf8');
+const alternateTemplate = fs.readFileSync('alternateTemplate.html', 'utf8');
+
+// Example data for rendering
+const data = {
+  pageTitle: 'Customizing Views with Mustache.js',
+  headerTitle: 'Welcome to My Website',
+  mainContent: 'This is the main content of the page.',
+  year: new Date().getFullYear(),
+};
+
+// Render the default template
+const renderedHtml = mustache.render(template, data);
+console.log('Default Template:\n', renderedHtml);
+
+// Render the alternate template
+const alternateRenderedHtml = mustache.render(alternateTemplate, data);
+console.log('\nAlternate Template:\n', alternateRenderedHtml);
